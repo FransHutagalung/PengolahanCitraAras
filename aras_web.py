de2f0d23ab34bfc0b3947271f3530b721e4f281b
FransHutagalung <adhimarnaek@gmail.com>
2025-05-02 12:11:15 +0700

    Create Procfile file

--- aras_web.py
+++ aras_web.py
@@ -1567,4 +1567,4 @@ def get_index_template():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
